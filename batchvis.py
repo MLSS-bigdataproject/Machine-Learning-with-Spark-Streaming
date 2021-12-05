53732e253f761c6f56e2dd822a6b603acf972f81
arjunvuppala123 <56397988+arjunvuppala123@users.noreply.github.com>
2021-12-05 22:20:19 +0530

    added batchwise plots

---
+++ batchvis.py
@@ -0,0 +1,34 @@
+import numpy as np 
+import pandas as pd
+import matplotlib.pyplot as plt
+
+df = pd.read_csv("batch.csv")
+plt.plot(df["LogRegAcc"], color = 'r')
+plt.plot(df["RandomForestAcc"], color = 'b')
+plt.plot(df["NaiveBayesAcc"], color = 'g')
+plt.xlabel("batch size")
+plt.ylabel("accuracy")
+plt.title("comparision of accuracy with varying batch size")
+plt.xticks(rotation=90)
+plt.legend(["Logistic Regression","Random Forest","Naive Bayes"], loc ="lower right")
+plt.show()
+
+plt.plot(df["LogRegPre"], color = 'r')
+plt.plot(df["RandomForestPre"], color = 'b')
+plt.plot(df["NaiveBayesPre"], color = 'g')
+plt.xlabel("batch size")
+plt.ylabel("Precision")
+plt.title("comparision of Precision with varying batch size")
+plt.xticks(rotation=90)
+plt.legend(["Logistic Regression" ,"Random Forest","Naive Bayes"], loc ="lower right")
+plt.show()
+
+plt.plot(df["LogRecRe"], color = 'r')
+plt.plot(df["RandomForestRe"], color = 'b')
+plt.plot(df["NaiveBayesRe"], color = 'g')
+plt.xlabel("batch size")
+plt.ylabel("Recall")
+plt.title("comparision of Recall with varying batch size")
+plt.xticks(rotation=90)
+plt.legend(["Logistic Regression" ,"Random Forest","Naive Bayes"], loc ="lower right")
+plt.show()
